d4821ac786e0df782d16233a35a0732b4082ce9a
Benjamin Barber <starworks5@gmail.com>
2023-02-28 09:25:43 -0800

    support for semitransparency

--- invokeai/newBackend/generateImage.js
+++ invokeai/newBackend/generateImage.js
@@ -54,20 +54,30 @@ function addPerlinNoise(image, mask, width, height){
 							let g2 = (maskPixelColor >> 8) & 0xff
 							let b2 = maskPixelColor & 0xff
 							let a2 = (maskPixelColor >> 24) & 0xff
-							if (r == 0 && g == 0 && b == 0 && a == 0 && r2 == 255 && 255 == 0 && b2 == 255 && a2 == 255){
+							if (r == 0 && g == 0 && b == 0 && a == 0 && r2 == 255 && g2 == 255 && b2 == 255 && a2 == 255){
 								let perlinNoise = Math.floor(Math.random() * 255)
 								r =  Math.floor(Math.random() * 255)
 								g =  Math.floor(Math.random() * 255)
 								b =  Math.floor(Math.random() * 255)
+								a =  25
+								image.setPixelColor(jimp.rgbaToInt(r, g, b, a), i, j)
+							}
+							else if ( a < 255 && r2 == 255 && 255 == 0 && b2 == 255 && a2 == 255){
+								let perlinNoise = Math.floor(Math.random() * 255)
+								let percent = a / 255
+								r =  math.floor((r  * percent ) + ( Math.floor(Math.random() * 255) * (1 - percent)))
+								g =  math.floor((g  * percent ) + ( Math.floor(Math.random() * 255) * (1 - percent)))
+								b =  math.floor((b  * percent ) + ( Math.floor(Math.random() * 255) * (1 - percent)))
 								a =  255
 								image.setPixelColor(jimp.rgbaToInt(r, g, b, a), i, j)
 							}
 						}
 					}
-				return image
+					image.write("addedPerlinNoise.png")
 				}
+			}).then((maskImage) => {
+				return maskImage
 			})
-			image.write("addedPerlinNoise.png")
 			console.log()
 			//console.log( maskImage.getPixelColor(0, 0))
 			return image
