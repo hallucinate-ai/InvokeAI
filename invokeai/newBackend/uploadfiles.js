c28c9c9a908850b79f5e337c98095283b591fdd1
Benjamin Barber <starworks5@gmail.com>
2023-03-15 06:32:09 -0700

    model downloads

--- invokeai/newBackend/uploadfiles.js
+++ invokeai/newBackend/uploadfiles.js
@@ -18,6 +18,7 @@ async function main() {
 		let model = Object.keys(downloadList)[i]
 		let modelURL = downloadList[model]
 		let src = modelURL
+		let downloadedFilesize = 0
 		let dst = "/tmp/models/"
 		if (!fs.existsSync(dst)) {
 			fs.mkdirSync(dst)
@@ -28,16 +29,26 @@ async function main() {
 		let s3Filesize = results.split(" ")[2]
 		console.log(cwd)
 		console.log(results)
-		dst = dst +  model + ".ckpt"
-		cmd = "wget -q -O " + dst + " " + src
+		cmd = "s3cmd --config=" + cwd + "/cw-object-storage-config_stable-diffusion ls s3://stable-diffusion-models/" + model + ".ckpt.done"
 		results = await execSync(cmd)
-		results = results.toString()
+		let done = results.toString()
 		console.log(cwd)
-		console.log(results)
-		let downloadedFilesize = fs.statSync(dst).size
-		console.log("s3Filesize: " + s3Filesize)
-		console.log("downloadedFilesize: " + downloadedFilesize)
-		if(fs.existsSync(dst) && s3Filesize != downloadedFilesize){
+		console.log(done)
+		if (done.length == 0) {
+			dst = dst +  model + ".ckpt"
+			cmd = "wget -q -O " + dst + " " + src
+			results = await execSync(cmd)
+			results = results.toString()
+			console.log(cwd)
+			console.log(results)
+			downloadedFilesize = fs.statSync(dst).size
+			console.log("s3Filesize: " + s3Filesize)
+			console.log("downloadedFilesize: " + downloadedFilesize)
+		}
+		else{
+			console.log("File already downloaded")
+		}
+		if(fs.existsSync(dst) && s3Filesize != downloadedFilesize && done.split("\n").length == 0){
 			cmd = "s3cmd --config="+cwd+"/cw-object-storage-config_stable-diffusion put " + dst +" s3://stable-diffusion-models/"+ model + ".ckpt"
 			results = await execSync(cmd)
 			results = results.toString()
@@ -47,10 +58,28 @@ async function main() {
 			results = await execSync(cmd)
 			results = results.toString()
 			console.log(cwd)
-			console.log(results)		
+			console.log(results)
+			cmd = "touch " + dst + ".done"
+			results = await execSync(cmd)
+			results = results.toString()
+			console.log(cwd)
+			cmd = "s3cmd --config="+cwd+"/cw-object-storage-config_stable-diffusion put " + dst +".done s3://stable-diffusion-models/"+ model + ".ckpt.done"
+			results = await execSync(cmd)
+			results = results.toString()
+			console.log(cwd)
+			console.log(results)
 		}
-		else{
+		else if (done.split("\n").length != 0){
 			console.log("File already exists")
+			cmd = "touch " + dst + ".done"
+			results = await execSync(cmd)
+			results = results.toString()
+			console.log(cwd)
+			cmd = "s3cmd --config="+cwd+"/cw-object-storage-config_stable-diffusion put " + dst +".done s3://stable-diffusion-models/"+ model + ".ckpt.done"
+			results = await execSync(cmd)
+			results = results.toString()
+			console.log(cwd)
+			console.log(results)
 		}
 	}
 }
