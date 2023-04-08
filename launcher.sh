84f6452e3ddda76944e6fc6b66ba87bd07ec884c
Benjamin Barber <starworks5@gmail.com>
2023-04-08 04:18:28 -0700

    dockerized and autostarted invoke

--- launcher.sh
+++ launcher.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-cd /app/InvokeAI/invokeai/newBackend
-node main.js &
 # change current working directory to frontend
 cd /app/InvokeAI/invokeai/frontend
 cwd = $(pwd)
 echo $cwd
 npm run dev -- --host &
-
+	
 trap killall INT TERM EXIT
 wait
